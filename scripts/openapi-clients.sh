f5f51653cab53f34f63ebd9f1b39aef18bc3a7d6
LoveIsGrief <loveisgrief@tuta.io>
2020-01-23 20:24:14 +0100

    Publish openapi clients to a mono-repo
    Before they would've been published to a repo per client

--- scripts/openapi-clients.sh
+++ scripts/openapi-clients.sh
@@ -4,17 +4,20 @@
 # API_LANGS
 #   A ':' delimited list of the client lib languages to be generated
 # API_GIT_USER
-#   The github username
-# API_REPO_PREFIX
-#   The prefix for the client lib repositories
+#   The git username
+# API_REPO
+#   The repository name
 # GIT_TOKEN
-#   A personal access token for github for pushing to repos
+#   A personal access token for github or gilab for pushing to repos
 #   !!!This is a secret and shouldn't be logged publicly!!!
 
 # (Optional environment vars)
 # ===========================
 # API_COMMIT_MSG
 #   A message to use when committing to the lib repo
+# API_REPO_HOST
+#   Whoever's hosting the repo e.g gitlab.com, github.com, etc.
+#   Default: framagit.org
 
 # Unofficial bash strict mode
 # https://web.archive.org/web/20190115051613/https://redsymbol.net/articles/unofficial-bash-strict-mode/
@@ -25,11 +28,12 @@ for lang in ${API_LANGS//:/ } ; do
 (
     echo "Generating client API libs for $lang"
 
-    out_dir="dist/api/$lang"
-    repo_id="${API_REPO_PREFIX}${lang}"
-    git_remote="https://${API_GIT_USER}:${GIT_TOKEN}@github.com/${API_GIT_USER}/${repo_id}.git"
-    if ! [ -e "$out_dir" ] ; then
-        git clone "https://github.com/${API_GIT_USER}/${repo_id}.git" "$out_dir"
+    out_repo="dist/api/${API_REPO}"
+    out_dir="${out_repo}/${lang}"
+    host_path="${API_REPO_HOST:framagit.org}/${API_GIT_USER}/${API_REPO}.git"
+    git_remote="https://${API_GIT_USER}:${GIT_TOKEN}@${host_path}"
+    if ! [ -e "$out_repo" ] ; then
+        git clone "https://${host_path}" "$out_repo"
     fi
 
     docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
@@ -38,13 +42,13 @@ for lang in ${API_LANGS//:/ } ; do
         -g "$lang" \
         --git-user-id "${API_GIT_USER}" \
         --git-repo-id "${repo_id}" \
-        -o "/local/$out_dir"
+        -o "/local/${out_dir}"
 
     # Docker uses root so we need to undo that
-    sudo chown -R `id -u` "$out_dir"
+    sudo chown -R `id -u` "$out_repo"
 
     # Commit and push changes to the remote
-    cd "$out_dir"
+    cd "$out_repo"
     git remote set-url origin "$git_remote"
     # Make sure something has changed
     if [[ `git status -s | wc -l` = 0 ]] ; then
@@ -52,7 +56,7 @@ for lang in ${API_LANGS//:/ } ; do
         continue
     fi
     git add .
-    git commit -m "${API_COMMIT_MSG:-"Minor update"}"
+    git commit -m "${API_COMMIT_MSG:-"Minor update $lang"}"
     git push
 )
 done
